d33c14d3d67263ba8243ee0008c086408f14a0b9
Tornike Despotashvili <95018466+Desp0o@users.noreply.github.com>
2025-04-15 17:23:31 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,15 +3,23 @@ import PackageDescription
 
 let package = Package(
     name: "StackWrapper",
-    platforms: [.iOS(.v16)],
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v10_15)
+    ],
     products: [
-        .library(name: "StackWrapper", targets: ["StackWrapper"])
+        .library(
+            name: "StackWrapper",
+            targets: ["StackWrapper"]
+        ),
     ],
     targets: [
-        .binaryTarget(
-            name: "StackWrapper",
-            url: "https://github.com/Desp0o/StackWrapper/releases/download/v1.1.0/StackWrapper.xcframework.zip",
-            checksum: "d9151368dd0d9a22b6172fbc14344f82b1b13ddb3467c79ce32e765119d9c484"
-        )
+        .target(
+            name: "StackWrapper"
+        ),
+        .testTarget(
+            name: "StackWrapperTests",
+            dependencies: ["StackWrapper"]
+        ),
     ]
 )
